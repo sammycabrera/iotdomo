ac5247e14b45890d56beb4b327a15104ef5b99c0
sammycabrera <sammy_cabrera@hotmail.com>
2024-03-07 14:41:35 -0500

    ok

--- domo_api/serializers.py
+++ domo_api/serializers.py
@@ -6,11 +6,13 @@ class LocationSerializer(serializers.Serializer):
     user_id = serializers.IntegerField()
 
 class DeviceSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     name_device=serializers.CharField(max_length=255)
     unidad=serializers.CharField(max_length=255)
     location_id=serializers.IntegerField()
 
 class DotSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     value=serializers.FloatField()
     datTime=serializers.DateTimeField()
     device_id=serializers.IntegerField()
